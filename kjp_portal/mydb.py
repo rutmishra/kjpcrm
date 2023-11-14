4039cfd2a4d1ba021e6d10e5204e71e849fb270b
rutmishra <ruturajm704@gmail.com>
2023-11-14 21:31:53 +0530

    Update mydb.py

--- kjp_portal/mydb.py
+++ kjp_portal/mydb.py
@@ -9,7 +9,7 @@ import mysql.connector
 dataBase = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    passwd = '@U9#5o!4.'
+    passwd = 'default'
 )
 
 #prepare a cursor object
